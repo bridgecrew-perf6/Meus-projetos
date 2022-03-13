c9e6e0d44c04d8429612384fff905c4f401d2729
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-13 20:27:04 -0300

    model for index page

--- project/backend/categories/urls.py
+++ project/backend/categories/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-    path('', categories_list, name='categories_list'),
     path('<slug:category_name>', category_view, name='category_view'),
 ]
--- project/backend/categories/views.py
+++ project/backend/categories/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render, redirect
 BP = 'pages/' # base path
 
 
-def categories_list(request):
-    return render(request, f'{BP}/categorias/index.html')
 
 
 def category_view(request, category_name):
