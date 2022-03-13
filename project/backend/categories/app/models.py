13f90bd3962fceda487be999f8f5d101aeabf415
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-12 21:46:18 -0300

    category model

--- project/backend/categories/app/models.py
+++ project/backend/categories/app/models.py
@@ -1,1 +1,9 @@
 from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField)
+
+
+
+class Category(Model):
+    name = CharField(max_length=128)
+    project_img = ImageField(upload_to='project/%Y/%m/%d')
+    project_url = TextField(blank=True, null=True)
+    github_url = TextField()
