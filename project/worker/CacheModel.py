84f7f09c5d3f1ff868cefffe0483dcc9e66eb3d3
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-16 19:44:31 -0300

    create cache model

---
+++ project/worker/CacheModel.py
@@ -0,0 +1,17 @@
+"""
+Cache model
+"""
+
+MODEL = {
+
+    'index': 'list categories html',
+    
+    'categorias.category_slug': {
+        'category_slug': 'list projects by category html',
+    },
+    
+    'projetos.project_slug': {
+        'project_slug': 'project html'
+    },
+    
+}
