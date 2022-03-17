8f952255a1fc07f5f92954228a5cba65577496e6
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-03-16 22:42:00 -0300

    favicon and cache views

--- project/backend/categories/views.py
+++ project/backend/categories/views.py
@@ -1,14 +1,13 @@
-from Fast.utils.main import get_cache_or_error, if_none, jsObj
-from django.shortcuts import get_object_or_404, render, redirect
-from backend.core import Project
-from . import Category
+from Fast.utils.main import get_cache_or_error, if_none
+from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 
 BP = 'pages/' # base path
 
 
 
-
+@cache_page(60*15)
 def category_view(request, category_slug):
     context = {'category_page': if_none(get_cache_or_error('categorias.category_slug').get(category_slug), '')}
     return render(request, f'{BP}/categorias/[category_slug].html', context)
--- project/backend/core/views.py
+++ project/backend/core/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import get_object_or_404, render, redirect
-from Fast.utils.main import get_cache_or_error, if_none, jsObj
-from backend.core import Project
+from django.shortcuts import render
+from Fast.utils.main import get_cache_or_error, if_none
+from django.views.decorators.cache import cache_page
 
 BP = 'pages/' # base path
 
 
+@cache_page(60*15)
 def index(request):
     context = {'category_list': get_cache_or_error('index')}
     return render(request, f'{BP}/index.html', context)
 
 
+@cache_page(60*15)
 def project_view(request, project_slug):
     context = {'project_page': if_none(get_cache_or_error('projetos.project_slug').get(project_slug), '')}
     return render(request, f'{BP}/projetos/[project_slug].html', context)
