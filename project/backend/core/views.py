88507eb197961457345e8155b97cfd4e14f9df11
Dhomini  Rabelo <dhominisalgado@gmail.com>
2022-04-01 13:51:51 -0300

    create cache

--- project/backend/core/views.py
+++ project/backend/core/views.py
@@ -5,13 +5,15 @@ from django.views.decorators.cache import cache_page
 BP = 'pages/' # base path
 
 
-@cache_page(60*5)
+# @cache_page(60*5)
 def index(request):
+    from worker.views.manager import create_cache
+    create_cache()
     context = {'category_list': get_cache_or_error('index')}
     return render(request, f'{BP}/index.html', context)
 
 
-@cache_page(60*5)
+# @cache_page(60*5)
 def project_view(request, project_slug):
     context = {'project_page': if_none(get_cache_or_error('projetos.project_slug').get(project_slug), '')}
     return render(request, f'{BP}/projetos/[project_slug].html', context)
